c94471bf6fc2234ff98e697f70fd22b67e99f2e7
Jesús <gjmg1612@gmail.com>
2019-06-27 08:19:12 -0500

    Now the server run in the port 8080

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,17 @@
 module.exports = {
     "env": {
         "browser": true,
-        "commonjs":true,
-        "es6": true, 
-        "node": true,
+        "commonjs": true,
+        "es6": true
     },
-    "extends": "airbnb-base",
+    "extends": "airbnb",
     "globals": {
         "Atomics": "readonly",
         "SharedArrayBuffer": "readonly"
     },
     "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+        "ecmaVersion": 2018
+    },
+    "rules": {
     }
 };
--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const app = express();
 const collectionRoutes = require('./routers/collection.router.js')
 const movieRoutes = require('./routers/movie.router.js');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/movie/', movieRoutes);
 app.use('/collection/', collectionRoutes);
 
-app.listen(3000,() => {
-  console.log('Server running in the port 3000');
+app.listen(8080,() => {
+  console.log('Server running in the port 8080');
 });
